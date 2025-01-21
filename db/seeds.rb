8b06adfb3327524a5a106f52b81ed29ec22550f0
Ilya <simonewarlet@gmail.com>
2025-01-21 17:11:34 +0200

    Add seeds

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+images_path = Rails.root.join('db', 'seed_images')
+puts images_path
+image_files = Dir[images_path.join('*')]
+puts image_files
+
+5.times do
+  puts 'here'
+  book = Book.new(
+    name: Faker::Book.title,
+    author_name: Faker::Book.author,
+    description: Faker::Lorem.paragraph(sentence_count: 5)
+  )
+
+  random_image = image_files.sample
+  book.image = File.open(random_image)
+
+  if book.save
+    puts "Book '#{book.name}' is saved."
+  else
+    puts "Cant save book: #{book.errors.full_messages.join(', ')}"
+  end
+end
+
+puts 'done'
