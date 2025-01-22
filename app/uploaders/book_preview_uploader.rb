d431ffe6ca4f44bcb9e6138ff89bb383e1ac70cb
Ilya <simonewarlet@gmail.com>
2025-01-22 11:41:43 +0200

    Remove comments

--- app/uploaders/book_preview_uploader.rb
+++ app/uploaders/book_preview_uploader.rb
@@ -1,8 +1,4 @@
 class BookPreviewUploader < CarrierWave::Uploader::Base
-  # Include RMagick, MiniMagick, or Vips support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  # include CarrierWave::Vips
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
