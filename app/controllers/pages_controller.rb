42192634d67bf327ace5f96a9c89a9b07e7c6784
Ilya <simonewarlet@gmail.com>
2025-01-23 10:38:27 +0200

    Add images to promo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,11 @@
 class PagesController < ApplicationController
-  def index; end
+  def index
+    @promos = [{ title: 'Esotericism and Numerology', image: '1.png' },
+               { title: 'Books in Ukrainian', image: '2.png' },
+               { title: 'Psychology and Relationships', image: '3.png' },
+               { title: 'Business Books', image: '4.png' },
+               { title: 'Fiction', image: '5.jpg' },
+               { title: 'Books for Parents', image: '6.jpg' },
+               { title: '123', image: '1.png' }]
+  end
 end
