637b07b33fb1e0ed878f67c5f1bc03d7bdbf08ae
Ilya <simonewarlet@gmail.com>
2025-01-23 15:57:06 +0200

    Fix book actions and add bookmark helpers

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,11 +1,19 @@
 module BooksHelper
-  def book_action_helper(_book, current_user)
-    if @book.available
-      button_to 'Take book', borrow_book_path(@book), method: :post, class: 'btn btn-primary'
-    elsif @book.histories.where(user: current_user, returned_at: nil).exists?
-      button_to 'Return book', return_book_path(@book), method: :post, class: 'btn btn-success'
+  def book_action_helper(book, current_user)
+    if book.available
+      button_to 'Take book', borrow_book_path(book), method: :post, class: 'btn btn-primary'
+    elsif book.histories.where(user: current_user, returned_at: nil).exists?
+      button_to 'Return book', return_book_path(book), method: :post, class: 'btn btn-success'
     else
       content_tag(:div, 'Sorry, book is occupied', { class: 'text-danger' })
+    end
+  end
+
+  def book_bookmark_helper(book, current_user)
+    if current_user.bookmarks.exists?(book: book)
+      button_to "<i class='bi bi-bookmark-fill'></i>".html_safe, book_bookmark_path(book), method: :delete
+    else
+      button_to "<i class='bi bi-bookmark'></i>".html_safe, book_bookmark_path(book), method: :post
     end
   end
 end
