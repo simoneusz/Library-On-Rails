ad6d1ce973f47bef1898771f818db260ce1bc7ab
Ilya <simonewarlet@gmail.com>
2025-01-24 15:38:14 +0200

    Remove comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,23 +2,18 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: %i[show edit update destroy]
   before_action :set_book, only: %i[create]
 
-  # GET /comments or /comments.json
   def index
     @comments = Comment.all
   end
 
-  # GET /comments/1 or /comments/1.json
   def show; end
 
-  # GET /comments/new
   def new
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
   def edit; end
 
-  # POST /comments or /comments.json
   def create
     @comment = @book.comments.new(comment_params.merge(user_id: current_user.id))
 
@@ -30,15 +25,12 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1 or /comments/1.json
   def update; end
 
-  # DELETE /comments/1 or /comments/1.json
   def destroy; end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = Comment.find(params[:id])
   end
@@ -47,7 +39,6 @@ class CommentsController < ApplicationController
     @book = Book.find_by(slug: params[:book_id])
   end
 
-  # Only allow a list of trusted parameters through.
   def comment_params
     params.require(:comment).permit(:content)
   end
